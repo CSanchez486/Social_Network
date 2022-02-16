b275c9244ec464cd3421aa309a23d066806fd904
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-15 22:24:03 -0500

    created teh app.use cases, need to create connection to test server.js

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3010;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log( `${PORT} is live.`);
+    });
+});
