83adec5d225fb4d2569d1f5ad0d7648696ebfb56
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-15 22:19:08 -0500

    defined const's needed for server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3010;
+const app = express();
+
