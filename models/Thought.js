faf5956559948deab1e8cb17e05feec55692dfbf
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-16 00:48:47 -0500

    created thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,27 @@
-// **Thought**:
+const { Schema, model } = require('mongoose');
+const User = require('./User');
 
-// * `thoughtText`
-//   * String
-//   * Required
-//   * Must be between 1 and 280 characters
+const userSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength:280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
 
-// * `createdAt`
-//   * Date
-//   * Set default value to the current timestamp
-//   * Use a getter method to format the timestamp on query
+    },
+)
 
-// * `username` (The user that created this thought)
-//   * String
-//   * Required
+
 
 // * `reactions` (These are like replies)
 //   * Array of nested documents created with the `reactionSchema`
