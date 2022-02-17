bcc71cda167696cbc99f75ca21a41965bc99ff22
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-16 21:16:30 -0500

    get single user by id controller created

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,16 +4,25 @@ module.exports = {
     // get all users
     getUsers(req, res) {
         User.find()
-        .then((users) => res.json(users))
+        .then((users) => res.status(200).json(users))
         .catch ((err) => res.status(500).json(err));
     },
+    // get one user by id (find one)
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('__v')
+        .then((user) => res.status(200).json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+    // create one user
+    createUser(req, res) 
 }
 
 
 
-// get one user by id (find one)
 
-// create one user
+
+
 
 // update user
 
