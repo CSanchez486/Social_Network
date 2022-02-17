7e1746474874a26cfd9b6645446b0c12c35248f0
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-17 10:12:16 -0500

    added router.use and module exports

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,3 +3,8 @@ const friendRoutes = require('./friendRoutes');
 const thoughtRoutes = require('./thoughtsRoutes');
 const userRoutes = require('./userRoutes');
 
+router.use('/friends', friendRoutes);
+router.use('/thought', thoughtRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
